78ed88e88307d7523754544c591b9bfa1c7296d6
eskylo <neto.s@msn.com>
2024-06-16 22:36:58 -0300

    Criar Controlador e Serviço Plano de Contas

--- src/plano-contas/plano-contas.controller.ts
+++ src/plano-contas/plano-contas.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Put, Param, Delete } from '@nestjs/common';
+import { PlanoContasService } from './plano-contas.service';
+import { CreatePlanoContasDto } from './dto/create-plano-contas.dto';
+import { UpdatePlanoContasDto } from './dto/update-plano-contas.dto';
 
 @Controller('plano-contas')
-export class PlanoContasController {}
+export class PlanoContasController {
+  constructor(private readonly planoContasService: PlanoContasService) {}
+
+  @Post()
+  create(@Body() createPlanoContasDto: CreatePlanoContasDto) {
+    return this.planoContasService.create(createPlanoContasDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.planoContasService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: number) {
+    return this.planoContasService.findOne(id);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: number, @Body() updatePlanoContasDto: UpdatePlanoContasDto) {
+    return this.planoContasService.update(id, updatePlanoContasDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: number) {
+    return this.planoContasService.remove(id);
+  }
+}
--- src/plano-contas/plano-contas.service.ts
+++ src/plano-contas/plano-contas.service.ts
@@ -1,4 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { PlanoContas } from './plano-contas.entity';
+import { CreatePlanoContasDto } from './dto/create-plano-contas.dto';
+import { UpdatePlanoContasDto } from './dto/update-plano-contas.dto';
 
 @Injectable()
-export class PlanoContasService {}
+export class PlanoContasService {
+  constructor(
+    @InjectRepository(PlanoContas)
+    private planoContasRepository: Repository<PlanoContas>,
+  ) {}
+
+  create(createPlanoContasDto: CreatePlanoContasDto): Promise<PlanoContas> {
+    const planoContas = this.planoContasRepository.create(createPlanoContasDto);
+    return this.planoContasRepository.save(planoContas);
+  }
+
+  findAll(): Promise<PlanoContas[]> {
+    return this.planoContasRepository.find();
+  }
+
+  findOne(id: number): Promise<PlanoContas> {
+    return this.planoContasRepository.findOneBy({ id });
+  }
+
+  async update(id: number, updatePlanoContasDto: UpdatePlanoContasDto): Promise<PlanoContas> {
+    await this.planoContasRepository.update(id, updatePlanoContasDto);
+    return this.findOne(id);
+  }
+
+  async remove(id: number): Promise<void> {
+    await this.planoContasRepository.delete(id);
+  }
+}
