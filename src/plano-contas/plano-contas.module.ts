0cd6cf088d048488fc31c81fd52cc5cbe65d90ea
eskylo <neto.s@msn.com>
2024-06-16 21:50:29 -0300

    Incluir ORM nos modulos

--- src/plano-contas/plano-contas.module.ts
+++ src/plano-contas/plano-contas.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { PlanoContasService } from './plano-contas.service';
 import { PlanoContasController } from './plano-contas.controller';
+import { PlanoContas } from './plano-contas.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([PlanoContas])],
   providers: [PlanoContasService],
-  controllers: [PlanoContasController]
+  controllers: [PlanoContasController],
 })
 export class PlanoContasModule {}
--- src/transacoes/transacoes.module.ts
+++ src/transacoes/transacoes.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TransacoesService } from './transacoes.service';
 import { TransacoesController } from './transacoes.controller';
+import { Transacao } from './transacao.entity';
+import { PlanoContas } from '../plano-contas/plano-contas.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Transacao, PlanoContas])],
   providers: [TransacoesService],
-  controllers: [TransacoesController]
+  controllers: [TransacoesController],
 })
 export class TransacoesModule {}
