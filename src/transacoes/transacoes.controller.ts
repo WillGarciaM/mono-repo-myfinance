7fb2e85c604df7ff9b7f81e2e2db019eb7a4fb94
eskylo <neto.s@msn.com>
2024-06-16 22:38:12 -0300

    Criar serviço e controlador Transações

--- src/transacoes/transacoes.controller.ts
+++ src/transacoes/transacoes.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Put, Param, Delete } from '@nestjs/common';
+import { TransacoesService } from './transacoes.service';
+import { CreateTransacaoDto } from './dto/create-transacao.dto';
+import { UpdateTransacaoDto } from './dto/update-transacao.dto';
 
 @Controller('transacoes')
-export class TransacoesController {}
+export class TransacoesController {
+  constructor(private readonly transacoesService: TransacoesService) {}
+
+  @Post()
+  create(@Body() createTransacaoDto: CreateTransacaoDto) {
+    return this.transacoesService.create(createTransacaoDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.transacoesService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: number) {
+    return this.transacoesService.findOne(id);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: number, @Body() updateTransacaoDto: UpdateTransacaoDto) {
+    return this.transacoesService.update(id, updateTransacaoDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: number) {
+    return this.transacoesService.remove(id);
+  }
+}
--- src/transacoes/transacoes.service.ts
+++ src/transacoes/transacoes.service.ts
@@ -1,4 +1,61 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Transacao } from './transacao.entity';
+import { CreateTransacaoDto } from './dto/create-transacao.dto';
+import { UpdateTransacaoDto } from './dto/update-transacao.dto';
+import { PlanoContas } from '../plano-contas/plano-contas.entity';
 
 @Injectable()
-export class TransacoesService {}
+export class TransacoesService {
+  constructor(
+    @InjectRepository(Transacao)
+    private transacaoRepository: Repository<Transacao>,
+    @InjectRepository(PlanoContas)
+    private planoContasRepository: Repository<PlanoContas>,
+  ) {}
+
+  async create(createTransacaoDto: CreateTransacaoDto): Promise<Transacao> {
+    const planoContas = await this.planoContasRepository.findOneBy({ id: createTransacaoDto.planoContasId });
+    if (!planoContas) {
+      throw new NotFoundException('Plano de contas não encontrado');
+    }
+    const transacao = this.transacaoRepository.create({ ...createTransacaoDto, planoContas });
+    return this.transacaoRepository.save(transacao);
+  }
+
+  findAll(): Promise<Transacao[]> {
+    return this.transacaoRepository.find({ relations: ['planoContas'] });
+  }
+
+  findOne(id: number): Promise<Transacao> {
+    return this.transacaoRepository.findOneBy({ id });
+  }
+
+  async update(id: number, updateTransacaoDto: UpdateTransacaoDto): Promise<Transacao> {
+    const transacao = await this.findOne(id);
+    if (!transacao) {
+      throw new NotFoundException('Transação não encontrada');
+    }
+
+    if (updateTransacaoDto.planoContasId) {
+      const planoContas = await this.planoContasRepository.findOneBy({ id: updateTransacaoDto.planoContasId });
+      if (!planoContas) {
+        throw new NotFoundException('Plano de contas não encontrado');
+      }
+      transacao.planoContas = planoContas;
+    }
+
+    Object.assign(transacao, updateTransacaoDto);
+    await this.transacaoRepository.save(transacao);
+    return transacao;
+  }
+
+  async remove(id: number): Promise<void> {
+    const transacao = await this.findOne(id);
+    if (!transacao) {
+      throw new NotFoundException('Transação não encontrada');
+    }
+    await this.transacaoRepository.delete(id);
+  }
+}
