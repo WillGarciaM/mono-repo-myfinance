2e2e505435ff009649384c6817a102ea8eef2d8a
eskylo <neto.s@msn.com>
2024-06-16 22:36:36 -0300

    Incluir Relação de Entidade

--- src/transacoes/dto/update-transacao.dto.ts
+++ src/transacoes/dto/update-transacao.dto.ts
@@ -1,4 +1,7 @@
-export class UpdateTransacaoDto {
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateTransacaoDto } from './create-transacao.dto';
+
+export class UpdateTransacaoDto extends PartialType(CreateTransacaoDto) {
     descricao?: string;
     data?: Date;
     planoContasId?: number;
