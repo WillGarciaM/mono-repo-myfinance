69b3c52626519fff80f40ab53971805838be3521
eskylo <neto.s@msn.com>
2024-06-22 15:58:50 -0300

    Adicionar config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PlanoContasModule } from './plano-contas/plano-contas.module';
 import { TransacoesModule } from './transacoes/transacoes.module';
+import { PlanoContas } from './plano-contas/plano-contas.entity';
+import { Transacao } from './transacoes/transacao.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.MYSQLDATABASE,
-      port: parseInt(process.env.MYSQLPORT),
-      username: process.env.MYSQLUSER,
-      password: process.env.MYSQLPASSWORD,
-      database: process.env.MYSQLDATABASE,
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('MYSQLHOST'),
+        port: configService.get<number>('MYSQLPORT'),
+        username: configService.get<string>('MYSQLUSER'),
+        password: configService.get<string>('MYSQLPASSWORD'),
+        database: configService.get<string>('MYSQLDATABASE'),
+        entities: [PlanoContas, Transacao],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
     }),
     PlanoContasModule,
     TransacoesModule,
